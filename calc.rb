32c5e20b0cbd5be2d247a5f5b1bc5f4298710efc
haradai <odawara@hyakuren.org>
2013-09-22 18:40:09 +0900

    d, rを排除。多重代入は全くバイト数は減らせない（=と改行がカンマ2つに変わるだけ）ので導入せず。

--- calc.rb
+++ calc.rb
@@ -4,11 +4,8 @@ P='TKJUO'
 U=15
 h={}
 l=0
-d=Date.today
 P.each_char{|c|h[c]=0}
-w=(Time.now.to_i-Time.new(d.year,d.month,d.day,9).to_i)/60
-w-=case w;when 0..240;0;when 240..660;60;when 660..780;75;else;90;end
+w=(Time.now.to_i-Date.today.to_time.to_i-32400)/60
+w-=case w;when 0..239;0;when 240..659;60;when 660..780;75;else;90;end
 $*[0].upcase.split(/\s*:\s*/).each{|e|(l+=$2.to_f;h[$1]=$2)if/([A-Z])\s*([0-9]+(.[0-9]+)?)/=~e&&h[$1]}
-r=''
-h.sort{|(k,v),(l,w)|P.index(k)<=>P.index(l)}.each{|k,v|m=(w*v.to_f/l).to_i;t=(m+q(m%U))/60.0;r<<k+"#{(t==t.to_i)?t.to_i: t}"}
-puts r
+h.sort{|(k,v),(l,w)|P.index(k)<=>P.index(l)}.each{|k,v|m=(w*v.to_f/l).to_i;t=(m+q(m%U))/60.0;print(k+"#{(t==t.to_i)?t.to_i: t}")}
